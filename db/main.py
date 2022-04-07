703fef1e355c38a3c0510a2835c687d5c2d7fd81
Fanfara1329 <t985978@gmail.com>
2022-04-07 21:10:11 +0300

    Первая работа

--- db/main.py
+++ db/main.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from data import db_session
-from data.__all_models import User
+from data.__all_models import User, Jobs
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -43,6 +43,15 @@ def main():
     user2.hashed_password = "qwerty"
     db_sess = db_session.create_session()
     db_sess.add(user2)
+    db_sess.commit()
+    job = Jobs()
+    job.team_leader = 1
+    job.job = 'deployment of residential modules 1 and 2'
+    job.work_size = 15
+    job.collaborators = '2, 3'
+    job.is_finished = False
+    db_sess = db_session.create_session()
+    db_sess.add(job)
     db_sess.commit()
     app.run()
 
